8ae1ed6ee156095aacd655e39de72753011c9fe1
GawarWawar <gawarwawar@gmail.com>
2024-10-11 18:11:58 +0300

    feat: Add models for the Project and Task

--- django_instance/schedule/models.py
+++ django_instance/schedule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    creation_date = models.DateTimeField(verbose_name="Date of Creation", default=datetime.datetime.now)
+    expire_date = models.DateTimeField(verbose_name="Date of expiration", null=True, default=None)
+
+class Task(models.Model):
+    project_instance = models.ForeignKey(Project, verbose_name="Project", on_delete=models.CASCADE)
+    description = models.CharField(verbose_name="Description", max_length=1000)
+    priority = models.IntegerField(verbose_name="Priority")
+    creation_date = models.DateTimeField(verbose_name="Date of Creation", default=datetime.datetime.now)
+    expire_date = models.DateTimeField(verbose_name="Date of expiration", null=True, default=None)
