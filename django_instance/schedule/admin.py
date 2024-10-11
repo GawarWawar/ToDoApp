962f257b81abc3f02b7bb2c83c6215898498873d
GawarWawar <gawarwawar@gmail.com>
2024-10-11 19:38:02 +0300

    feat(admin): Give list_display to each admin with every field except id

--- django_instance/schedule/admin.py
+++ django_instance/schedule/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from schedule import models
 
 class ProjectAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["user_instance", "name", "creation_date", "expire_date"]
     
 class TaskAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["project_instance", "description", "priority", "creation_date", "expire_date"]
 
 # Register your models here.
 admin.site.register(models.Project, ProjectAdmin)
