a7467b1a0ecce09ffb3d039aba5cb7b1ce23ae6e
GawarWawar <gawarwawar@gmail.com>
2024-10-15 02:49:15 +0300

    fix(main page): Now have propper functionality in updating task after creating one
    - Create createTask in tasks.js to create xhttp request for task creation -> listner was deactivated

--- django_instance/schedule/static/listeners.js
+++ django_instance/schedule/static/listeners.js
@@ -1,18 +1,18 @@
-document.addEventListener("submit", (event) => {
-  // on form submission, prevent default
-  event.preventDefault();
-  const { target } = event;
+// document.addEventListener("submit", (event) => {
+//   // on form submission, prevent default
+//   event.preventDefault();
+//   const { target } = event;
 
-  let xhttp = new XMLHttpRequest();
+//   let xhttp = new XMLHttpRequest();
 
-  xhttp.withCredentials = "true";
-  xhttp.open(
-    target.getAttribute("method"),
-    target.getAttribute("action"),
-    true
-  );
-  xhttp.setRequestHeader("X-CSRFToken", "{{ csrf_token }}");
+//   xhttp.withCredentials = "true";
+//   xhttp.open(
+//     target.getAttribute("method"),
+//     target.getAttribute("action"),
+//     true
+//   );
+//   xhttp.setRequestHeader("X-CSRFToken", "{{ csrf_token }}");
 
-  const formData = new FormData(target);
-  xhttp.send(formData);
-});
+//   const formData = new FormData(target);
+//   xhttp.send(formData);
+// });
