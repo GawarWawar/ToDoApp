c5a7439374478f3a9b58a0c3bb2b8ec7e927832c
GawarWawar <gawarwawar@gmail.com>
2024-10-14 21:48:42 +0300

    feat(views): Create endpoints for task and project to retrieve and create via get and post requests

--- django_instance/schedule/urls.py
+++ django_instance/schedule/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("projects/<project_id>/tasks/create", views.task)
+    path("projects/", views.project_endpoint),
+    path("projects/<project_id>/tasks/", views.task_endpoint),
+    path("projects/<project_id>/tasks/create", views.task_endpoint)
 ]
--- django_instance/schedule/views.py
+++ django_instance/schedule/views.py
@@ -19,8 +19,49 @@ def get_users_projects(request):
     task_collection_json = json.dumps(all_tasks)
     return render(request, "index.html", {"data": task_collection_json})
 
+decorators.login_required
+def project_endpoint(request):
+    if request.method == "GET":
+        projects = models.Project.objects.filter(user_instance = request.user)
+        all_projects = []
+        for project in projects:
+            proj_dict = project.convert_time_field_to_json()
+            all_projects.append(proj_dict)
+        context = json.dumps(all_projects)
+        print(context)
+    
+    elif request.method == "POST":
+
+        # new_project = models.Project.objects.create(
+        #     user_instance = request.user,
+        #     name = request.project["name"]
+        # )
+        
+        greating_string = f"Hi"
+    return HttpResponse(context)
+        
+
 @decorators.login_required
 @csrf_exempt
-def task(request, project_id):
-    return HttpResponse()
+def task_endpoint(request, project_id):
+    if request.method == "GET":
+        project_instance = models.Project.objects.get(id = project_id)
+        
+        tasks = models.Task.objects.filter(project_instance = project_instance)
+        all_tasks = []
+        for task in tasks:
+            proj_dict = task.convert_time_field_to_json()
+            all_tasks.append(proj_dict)
+        context = json.dumps(all_tasks)
+        print(context)
+    if request.method == "POST":
+        # project_instance = models.Project.objects.get(id = project_id)
+        # new_task = models.Task.objects.create(
+        #     project_instance = project_instance,
+        #     description = request.POST["description"],
+        #     priority = 0,
+        # )
+        context = f"Hi, {request.POST["description"]}"
+        print(context)
+    return HttpResponse(context)
 
