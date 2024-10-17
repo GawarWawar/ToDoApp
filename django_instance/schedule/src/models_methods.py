427708660f549a22cd6783b5f98e89479d6c3ae3
GawarWawar <gawarwawar@gmail.com>
2024-10-17 11:34:58 +0300

    fix(models method): Returns empty dict when ObjectDoesNotExist

--- django_instance/schedule/src/models_methods.py
+++ django_instance/schedule/src/models_methods.py
@@ -1,6 +1,7 @@
 import abc
-from datetime import datetime
 from django_instance.settings import JS_TIME_FORMAT
+from django.core.exceptions import ObjectDoesNotExist
+
 
 class ModelsUtils():
     @classmethod
@@ -16,7 +17,11 @@ class ModelsWithTimeFIelds(ModelsUtils):
             return None
         
     def dict_with_convert_time_field_to_json(self,  TIME_FORMAT = JS_TIME_FORMAT):
-        self_dict_to_json = self.to_dict()
+        try:
+            self_dict_to_json = self.to_dict()
+        except ObjectDoesNotExist:
+            return {}
+        
         for key in self_dict_to_json:
             if "date" in key:
                 self_dict_to_json[key] = self.time_field_to_string(key, TIME_FORMAT=TIME_FORMAT)
