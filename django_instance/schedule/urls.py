38992348cc5c61a409bedaa29f1e896d2bc76b20
GawarWawar <gawarwawar@gmail.com>
2024-10-21 15:55:22 +0300

    feat(urls): Make urls more suitable for htmx structure: - add name for every url - add endpoint to get full project details

--- django_instance/schedule/urls.py
+++ django_instance/schedule/urls.py
@@ -7,14 +7,15 @@ from schedule.views.tasks_endpoints import project_tasks_endpoint
 
 
 urlpatterns = [
-    path("projects/", projects_endpoint),
-    path('projects/create', projects_endpoint),
+    path("projects/", projects_endpoint, name="projects"),
+    path('projects/create', projects_endpoint, name="new_project"),
     
-    path('projects/<project_id>/edit', project_endpoint),
-    path('projects/<project_id>/delete', project_endpoint),
+    path('projects/<project_id>', project_endpoint, name="project_details"),
+    path('projects/<project_id>/edit', project_endpoint, name="edit_project"),
+    path('projects/<project_id>/delete', project_endpoint, name="delete_project"),
     
-    path("projects/<project_id>/tasks/", project_tasks_endpoint),
-    path("projects/<project_id>/tasks/create", project_tasks_endpoint),
+    path("projects/<project_id>/tasks/", project_tasks_endpoint, name="project_tasks"),
+    path("projects/<project_id>/tasks/create", project_tasks_endpoint, name="create_task_for_project"),
     
     path("projects/<project_id>/tasks/<task_id>/edit", project_task_endpoint),
     path("projects/<project_id>/tasks/<task_id>/delete", project_task_endpoint),
